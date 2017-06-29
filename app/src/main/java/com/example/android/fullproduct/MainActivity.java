e6e3705a4c632b3e8c449a122ea8a13d9f8110ab
kowa1ski <kowalski@gmx.es>
2017-06-29 06:05:36 +0200

    intent a la nueva actividad funcinando

--- app/src/main/java/com/example/android/fullproduct/MainActivity.java
+++ app/src/main/java/com/example/android/fullproduct/MainActivity.java
@@ -1,5 +1,6 @@
 package com.example.android.fullproduct;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
@@ -22,6 +23,8 @@ public class MainActivity extends AppCompatActivity {
         fab.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
+                Intent intent = new Intent(MainActivity.this, Editor.class);
+                startActivity(intent);
                 Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
                         .setAction("Action", null).show();
             }
