f2019cdf575baf8b7cbde33872d2c9de9b5e6a7f
kowa1ski <kowalski@gmx.es>
2017-07-28 17:13:07 +0200

    OncreateLoader completado

--- app/src/main/java/com/example/android/fullproduct/Editor.java
+++ app/src/main/java/com/example/android/fullproduct/Editor.java
@@ -1,21 +1,70 @@
 package com.example.android.fullproduct;
 
 import android.database.Cursor;
+import android.net.Uri;
 import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
 import android.support.v4.app.LoaderManager;
+import android.support.v4.content.CursorLoader;
 import android.support.v4.content.Loader;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.view.View;
+import android.widget.EditText;
+
+import com.example.android.fullproduct.data.ProductContract;
 
 public class Editor extends AppCompatActivity implements LoaderManager.LoaderCallbacks<Cursor> {
+
+
+    // Identifier for the product data loader
+    private static final int EXISTING_PRODUCT_LOADER = 0;
+
+    /**
+     * Content URI for the existing product (null if it´s a new pet)
+     */
+    private Uri mCurrentProductUri;
+
+    /**
+     * EditText field to enter the name of product
+     */
+    private EditText mNameProduct;
+
+    /**
+     * EditText field to enter the quantity
+     */
+    private EditText mQuantityProduct;
+
+
+
+
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_editor);
+
+        // Find all relevant views that we will need to read user input from
+        mNameProduct = (EditText) findViewById(R.id.productName);
+        mQuantityProduct = (EditText) findViewById(R.id.quantityOfProducts);
+
+        // Setup OnTouchListeners on all the input fields, so we can determine if the user
+        // has touched or modified them. This will let us know if there are unsaved changes
+        // or not, ir the user tries to leave the editor without saving.
+        // TODO están en la línea 158 del pets
+
+
+
+
+
+
+
+
+
+
+
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
 
@@ -27,11 +76,47 @@ public class Editor extends AppCompatActivity implements LoaderManager.LoaderCal
                         .setAction("Action", null).show();
             }
         });
+
+
     }
+
+
+
+
+
+
+    /**
+     *
+     */
+
+
+
+
+
+
 
     @Override
     public Loader<Cursor> onCreateLoader(int id, Bundle args) {
-        return null;
+        // Since the editor shows all pet attributes, define a projection that contains
+        // all columns from the product table.
+        String[] projection = {
+                ProductContract.ProductEntry._ID,
+                ProductContract.ProductEntry.COLUMN_PRODUCT_NAME,
+                ProductContract.ProductEntry.COLUMN_QUANTITY,
+                ProductContract.ProductEntry.COLUMN_PRICE,
+                ProductContract.ProductEntry.COLUMN_IMAGE
+        };
+
+        // This loader will execute the ContentProvider´s query method on a background thread
+        return new CursorLoader(this,       // Parent activity context
+                mCurrentProductUri,         // Query the content URI for the current product
+                projection,                 // Columns to include in the resulting Cursor
+                null,                       // No selection clause
+                null,                       // No selection arguments
+                null                        // Default sort order
+
+        );
+
     }
 
     @Override
