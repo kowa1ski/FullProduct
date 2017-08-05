d7dadfdd8025a8df0055c0b3fcd2cd0b38b15f0e
kowa1ski <kowalski@gmx.es>
2017-08-05 18:55:22 +0200

    Comprobar si la base se crea o no y por qué no se muestra

--- app/src/main/java/com/example/android/fullproduct/MainActivity.java
+++ app/src/main/java/com/example/android/fullproduct/MainActivity.java
@@ -1,16 +1,35 @@
 package com.example.android.fullproduct;
 
+import android.content.ContentUris;
 import android.content.Intent;
+import android.database.Cursor;
+import android.net.Uri;
 import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
+
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.view.View;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.widget.AdapterView.OnItemClickListener;
+import android.widget.ListView;
 
-public class MainActivity extends AppCompatActivity {
+import android.app.LoaderManager;
+import android.content.CursorLoader;
+import android.content.Loader;
+
+import android.widget.AdapterView;
+import android.widget.ListView;
+
+import com.example.android.fullproduct.data.ProductContract;
+
+
+public class MainActivity extends AppCompatActivity implements LoaderManager.LoaderCallbacks<Cursor> {
+
+    private static final int PRODUCT_LOADER = 0 ;
+    ProductCursorAdapter mCursorAdapter;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -29,10 +48,48 @@ public class MainActivity extends AppCompatActivity {
                         .setAction("Action", null).show();
             }
         });
+        // TODO continuar con lo del otro en la línea 53 de las pet añadiendo el list
+        // Vamos alla
+        // Find the ListView which will be populated with the product data
+        ListView productListView = (ListView) findViewById(R.id.list);
+
+        //Setup an Adapter to create a list item for each row of product data in the Cursor.
+        // There is no product data yet (until the loader finishes) so pass in null for he Cursor.
+        mCursorAdapter = new ProductCursorAdapter(this, null);
+        productListView.setAdapter(mCursorAdapter);
+
+        //Setup item click listener
+        productListView.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+
+            @Override
+            public void onItemClick(AdapterView<?> adapterView, View view, int position, long id) {
+
+                //Create a new intent to go to [@link EditorActivity}
+                Intent intent = new Intent(MainActivity.this, Editor.class);
+
+                //Form the content URI that represents the specific product that was clicked on,
+                // by appending the "id" (passed as input to this method) onto the
+                // {@link ProductEntry#CONTENT_URI).
+                // For example, the URI would be "content://com.example.android.product/product/2"
+                // if the product with ID 2 was clicked on.
+                Uri currentProductUri = ContentUris.withAppendedId(ProductContract.ProductEntry.CONTENT_URI, id);
+
+                // Set the URI on the data field of the intent
+                intent.setData(currentProductUri);
+
+                // Launch the {@link Editor} to display the data for the current product
+                startActivity(intent);
+
+            }
+        });
+
+        // Kick off the loader
+        getLoaderManager().initLoader(PRODUCT_LOADER, null, this);
     }
 
 
-    // TODO continuar con lo del otro en la línea 53 de las pet añadiendo el list
+
+
 
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
@@ -54,5 +111,38 @@ public class MainActivity extends AppCompatActivity {
         }
 
         return super.onOptionsItemSelected(item);
+    }
+
+    @Override
+    public Loader<Cursor> onCreateLoader(int id, Bundle args) {
+        // Define a projection that specifies the columns from the table we care about.
+        String [] proyection = {
+                ProductContract.ProductEntry._ID,
+                ProductContract.ProductEntry.COLUMN_PRODUCT_NAME,
+                ProductContract.ProductEntry.COLUMN_QUANTITY
+        };
+
+        // This loader will execute the ContentProvider´s query method on a background thread.
+        return new CursorLoader( this,                      // Parent activity context
+                ProductContract.ProductEntry.CONTENT_URI,   // Provider content URI to query
+                proyection,                                 // Columns to include in the resulting Cursor
+                null,                                   // no selection clause
+                null,                                   // no selection arguments
+                null                                    // Default sort order
+        );
+    }
+
+    @Override
+    public void onLoadFinished(Loader<Cursor> loader, Cursor data) {
+        //Update {@link ProductCursorAdapter} with this new cursor containing updated product data.
+        mCursorAdapter.swapCursor(data);
+
+    }
+
+    @Override
+    public void onLoaderReset(Loader<Cursor> loader) {
+        //Callback called when the data needs to be deleted
+        mCursorAdapter.swapCursor(null);
+
     }
 }
